ae735e3ee31eec061c9abc9228c820476fd90a4c
Lodhammar <58824930+Lodhammar@users.noreply.github.com>
2021-06-10 00:27:27 +0100

    quirky leveling system

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from discord import FFmpegPCMAudio
 from dhooks import Webhook
 import requests
 from discord.ext import commands
+import json
+import os
 
 
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix = '!', intents=intents)
+os.chdir(r'C:\Users\seanl\Documents\pythonbot')
 
 
 
@@ -18,7 +21,6 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Shrek"))
 
 
-
 @client.event
 async def on_member_join(member):
     guild = client.get_guild(837400158839898122)
@@ -26,8 +28,44 @@ async def on_member_join(member):
     await channel.send(f'Welcome to the server {member.mention}!')
     await member.send(f'Welcome to the {guild.name}, {member.name}!')
 
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
 
 
+
+@client.event 
+async def on_message(message):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, message.author)
+    await add_experience(users, message.author, 5)
+    await level_up(users, message.author, message.channel)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
+
+async def update_data(users, user):
+    if not user.id in users:
+        users[user.id] = {}
+        users[user.id]['experience'] = 0
+        users[user.id]['level'] = 1
+
+async def add_experience(users, user, exp):
+    users[user.id]['experience'] += exp
+
+async def level_up(users, user, channel):
+    experience = users[user.id]['experience']
+    lvl_start = users[user.id]['level']
+    lvl_end = int(experience ** (1/4))
+
+    if lvl_start < lvl_end:
+        await client.send(channel, '{} has leveled up to level {}'.format(user.mention), lvl_end)
 
 @client.command(pass_context = True)
 async def join(ctx):
@@ -60,6 +98,7 @@ async def nick(ctx, member: discord.Member, nick):
 @client.command(pass_context=True)
 async def info(ctx):
     await ctx.send("You can communicate with people on the server and talk with them in voicechat.")
+
 
 
 with open('token.txt', 'r', encoding='utf-8') as f:
