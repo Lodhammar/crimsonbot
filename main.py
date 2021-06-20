c5f38fcdc85e712eec30cb50d246bac36d08ac2c
Lodhammar <58824930+Lodhammar@users.noreply.github.com>
2021-06-20 14:47:22 +0100

    calc testing

--- main.py
+++ main.py
@@ -74,8 +74,16 @@ async def button(ctx):
     await ctx.send(
         "Calculator",
         components = [
-            Button(label = 9)
-          
+            Button(label = 9),
+            Button(label = 8),
+            Button(label = 7),
+            Button(label = 6),
+            Button(label = 5),
+            Button(label = 4),
+            Button(label = 3),
+            Button(label = 2),
+            Button(label = 1),
+            Button(label = 0)
         ]
     )
 
